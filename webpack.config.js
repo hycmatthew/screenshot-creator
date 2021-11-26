9ca005bdb882ca00d56320a2ef393a10c7c623b8
hycmatthew <43464970+hycmatthew@users.noreply.github.com>
2021-11-27 00:49:32 +0800

    update mui

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ const resolvePath = _path => path.resolve(__dirname, _path);
 module.exports = {
     entry: resolvePath('./src/index.js'),
     output: {
-        path: resolvePath('/dist'),
+        path: path.join(__dirname,'/dist'),
         filename: 'index.bundle.js'
     },
     devServer:{
